b3eee8ec75dca56b185b359f2a1a8581c5fe51f7
Yannick Gloster <yannickgloster@gmail.com>
2020-12-13 16:49:14 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from logging.config import fileConfig
 from typing import List, Dict, Tuple
 from utils.server import WebServer
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __dev__ = 784871645483237386
 
 
